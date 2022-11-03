70d226e5b4ca30d2332be41f6b0bd6c1e8b33880
AlexFreik <alexfreik5@gmail.com>
2022-11-03 15:14:08 +0100

    env maps init

--- src/render.cpp
+++ src/render.cpp
@@ -2,10 +2,56 @@
 #include "intersect.h"
 #include "light.h"
 #include "screen.h"
+#include "texture.h"
 #include <framework/trackball.h>
 #ifdef NDEBUG
 #include <omp.h>
 #endif
+
+Image envImage(std::filesystem::path { DATA_DIR } / "cube_map.jpg");
+
+glm::vec2 getEnvironmentTexelCoords(glm::vec3 p)
+{
+    auto absX = std::fabs(p.x);
+    auto absY = std::fabs(p.y);
+    auto absZ = std::fabs(p.z);
+
+    glm::vec2 sideCoords;
+
+    glm::vec2 center { -1, -1 }, shift { -1, -1 };
+    if (absX >= absY && absX >= absZ) {
+        if (p.x > 0) { // face 0
+            center = { 2.5f / 4, 1.5f / 3 };
+            shift = { -p.z / 8, p.y / 6 };
+        } else { // face 1
+            center = { 0.5f / 4, 1.5f / 3 };
+            shift = { p.z / 8, p.y / 6 };
+        }
+    }
+
+    if (absY >= absX && absY >= absZ) {
+        if (p.y > 0) { // face 2
+            center = { 1.5f / 4, 0.5f / 3 };
+            shift = { p.x / 8, -p.z / 6 };
+        } else { // face 3
+            center = { 1.5f / 4, 2.5f / 3 };
+            shift = { p.x / 8, p.z / 6 };
+        }
+    }
+
+    if (absZ >= absX && absZ >= absY) {
+        if (p.z > 0) { // face 4
+            center = { 1.5f / 4, 1.5f / 3 };
+            shift = { p.x / 8, p.y / 6 };
+        } else { // face 5
+            center = { 3.5f / 4, 1.5f / 3 };
+            shift = { -p.x / 8, p.y / 6 };
+        }
+    }
+    auto ans = center + shift;
+    assert(0 <= ans.x && ans.x <= 1 && 0 <= ans.y && ans.y <= 1);
+    return ans;
+}
 
 glm::vec3 getFinalColor(const Scene& scene, const BvhInterface& bvh, Ray ray, const Features& features, int rayDepth)
 {
@@ -17,17 +63,17 @@ glm::vec3 getFinalColor(const Scene& scene, const BvhInterface& bvh, Ray ray, co
         if (features.enableRecursive) {
             Ray reflection = computeReflectionRay(ray, hitInfo);
             // Verifying if the ray intersects a specular surface and if the rayDepth is less than 5
-            if(hitInfo.material.ks != glm::vec3 {0.0, 0.0, 0.0} && rayDepth < 5)
+            if (hitInfo.material.ks != glm::vec3 { 0.0, 0.0, 0.0 } && rayDepth < 5)
                 // Adding the reflected light with consideration to the specularity
                 Lo += hitInfo.material.ks * getFinalColor(scene, bvh, reflection, features, rayDepth + 1);
         }
 
         // Draw a ray of the color of the surface if it hits the surface and the shading is enabled.
-        if(features.enableShading) {
+        if (features.enableShading) {
             drawRay(ray, Lo);
         }
         // Draw a black ray if the shading is disabled.
-        else{
+        else {
             drawRay(ray, glm::vec3(0.0, 0.0, 0.0));
         }
 
@@ -38,6 +84,14 @@ glm::vec3 getFinalColor(const Scene& scene, const BvhInterface& bvh, Ray ray, co
 
         drawRay(ray, glm::vec3(1.0f, 0.0f, 0.0f));
         // Set the color of the pixel to black if the ray misses.
+
+        if (features.extra.enableEnvironmentMapping) {
+            AxisAlignedBox envBox = { { -64, -64, -64 }, { 64, 64, 64 } };
+            if (intersectRayWithShape(envBox, ray)) {
+                auto texelCoords = getEnvironmentTexelCoords((ray.origin + ray.direction * ray.t) / 64.0f);
+                return acquireTexel(envImage, texelCoords, features);
+            }
+        }
         return glm::vec3(0.0f);
     }
 }
